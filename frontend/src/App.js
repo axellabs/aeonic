3595d651fdf0cd8ff16fb811c26268a4272086fe
Axel Simms <mail@axelsimms.com>
2021-01-27 16:40:23 -0500

    init project with create react app and a skeletal ui

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        Aeonic Notes
+      </header>
+      <div className="markdown">
+        <div className="editor">
+          markdown editor
+        </div>
+        <div className="view">
+          markdown view
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
