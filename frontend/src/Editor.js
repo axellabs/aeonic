7c3bdfe9bcefb75c76f333aa025727de25d231c1
Axel Simms <mail@axelsimms.com>
2021-01-27 16:58:25 -0500

    added a editable text area

--- frontend/src/Editor.js
+++ frontend/src/Editor.js
@@ -4,11 +4,21 @@ import parse from 'remark-parse'
 import remark2react from 'remark-react'
 
 class Editor extends React.Component {
+  constructor() {
+    super()
+    this.state = { text: "# hello world" }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(e) {
+    this.setState({ text: e.target.value })
+  }
+
   render() {
     return (
       <div className="markdown">
         <div className="editor">
-          markdown editor
+          <textarea className="editor-form" value={this.state.text} onChange={this.onChange} />
         </div>
         <div className="view">
           markdown view
@@ -18,4 +28,4 @@ class Editor extends React.Component {
   }
 }
 
-export default Editor;
+export default Editor
