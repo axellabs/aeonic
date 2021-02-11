5ee19c705a37817f35a2c268478655515f4d493c
Axel Simms <mail@axelsimms.com>
2021-02-10 21:53:58 -0500

    added sidebar and cleaned up the code

--- frontend/src/Editor.js
+++ frontend/src/Editor.js
@@ -1,73 +1,36 @@
 import React from 'react'
-import ReactCodeMirror from 'react-codemirror';
 
-import 'codemirror/lib/codemirror.css';
-import 'hypermd/mode/hypermd.css';
-import 'hypermd/theme/hypermd-light.css';
-import './hypermd.css'
-
-import 'codemirror/lib/codemirror';
-
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/markdown/markdown';
-import 'codemirror/mode/gfm/gfm';
-
-import 'hypermd/core';
-import 'hypermd/mode/hypermd';
-
-import 'hypermd/addon/hide-token';
-import 'hypermd/addon/cursor-debounce';
-import 'hypermd/addon/fold';
-import 'hypermd/addon/read-link';
-import 'hypermd/addon/click';
-import 'hypermd/addon/hover';
-import 'hypermd/addon/paste';
-import 'hypermd/addon/insert-file';
-import 'hypermd/addon/mode-loader';
-import 'hypermd/addon/table-align';
+const CodeMirror = require("codemirror")
+const HyperMD = require("hypermd")
 
 class Editor extends React.Component {
-  constructor() {
-    super()
-    this.state = { text: "# hello world" }
-    this.onChange = this.onChange.bind(this)
-
-    this.codeMirrorRef = React.createRef();
+  constructor(props) {
+    super(props)
+    this.editorAreaRef = React.createRef()
   }
 
-  onChange(e) {
-    this.setState({ text: e.target.value })
+  setOptions() {
+    const options = {
+      lineNumbers: false,
+      lineWrapping: true,
+      hmdHideToken: true
+    }
+    return options
+  }
+
+  componentDidMount() {
+    let options = this.setOptions()
+    let cm = CodeMirror(this.editorAreaRef.current)
+    HyperMD.switchToHyperMD(cm, options)
   }
 
   render() {
-    const options = {
-      mode: 'hypermd',
-      theme: 'hypermd-light',
-
-      hmdFold: {
-        image: true,
-        link: true,
-        math: true,
-      },
-      hmdHideToken: true,
-      hmdCursorDebounce: true,
-      hmdPaste: true,
-      hmdClick: true,
-      hmdHover: true,
-      hmdTableAlign: true,
-    }
-
-    const defaultText = "# hello world\n\n This is a test document."
 
 
     return (
-      <div className="markdown">
-        <ReactCodeMirror
-          className="editor"
-          value={defaultText}
-          ref={this.codeMirrorRef}
-          options={options} />
+      <div className="app-editor">
+        <div className="app-editor--cover-image" />
+        <div className="app-editor--area" ref={this.editorAreaRef} />
       </div>
     )
   }
